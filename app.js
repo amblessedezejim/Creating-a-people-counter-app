05e9f98e352302d27110dd905af22396df73f012
Amblessed Ezejim <amblessedezejim@gmail.com>
2023-01-10 19:57:26 +0100

    Add increment function

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const welcomeEl = document.querySelector("#welcome-el");
+const incrementBtn = document.querySelector("#increment-btn");
+const countEl = document.querySelector("#count-el");
+
 let username = "Ezejim Amblessed";
 welcomeEl.innerText = `Welcome Back, ${username}`;
+
+let count = 0;
+incrementBtn.addEventListener("click", () => {
+  count++;
+  countEl.textContent = count;
+});
